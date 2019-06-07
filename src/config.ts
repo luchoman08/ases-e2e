f6c07da5bb68fcbc76792c467c03474c25f39130
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-06-07 18:55:30 -0500

    feat: added support for get logged driver

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
-export const baseUrl = "http://192.168.117.40/moodle35_ases";
+export const baseUrl = "http://localhost/moodle35";
 export const courseId = "25643";
 export const instanceId = "450299";
+export const userName = "sistemas1008";
+export const password = "pilos%2015";
--- src/driver.ts
+++ src/driver.ts
@@ -1,10 +1,17 @@
-import { Builder } from "selenium-webdriver";
+import { Builder, WebDriver, By, Capabilities } from "selenium-webdriver";
+import { 
+    baseUrl,
+    userName,
+    password as globalPassword
+} from "./config";
 var initedDriver = false;
-var driver = null; 
-let baseUrl = "http://192.168.117.40/moodle35_ases";
+var driver: WebDriver;
 
-async function initDriver() {
-    driver = await new Builder().forBrowser('firefox').build();
+async function initDriver(width? : number) {
+    
+    driver = await new Builder()
+    .forBrowser('firefox')
+    .build();
     await driver.get(baseUrl);
     initedDriver = true;
 } 
@@ -12,6 +19,24 @@ export async function getDriver() {
     if(!initedDriver) {
         await initDriver();
     }
+    return driver;
+}
+export async function getLoggedDriver(username?: string, password?: string, width?: number) {
+    if(!initedDriver) {
+        await initDriver();
+    }
+    await driver.navigate().to(`${baseUrl}/login/index.php`);
+    await driver
+    .findElement(By.id('username'))
+    .sendKeys(username? username: userName);
+    
+    await driver
+    .findElement(By.id('password'))
+    .sendKeys(password? password: globalPassword);
+    var logginButton = await driver
+    .findElement(By.xpath('//form/*[@id="loginbtn"]'));
+    console.log(logginButton, 'login button');
+    await logginButton.click();
     return driver;
 }
 
